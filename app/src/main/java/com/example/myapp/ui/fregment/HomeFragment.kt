e91497d07eb310f1d9480ea635424603615e7f33
azzadpandit1 <azzad.pandit@aurosociety.org>
2023-08-18 17:47:30 +0530

    network call  done

--- app/src/main/java/com/example/myapp/ui/fregment/HomeFragment.kt
+++ app/src/main/java/com/example/myapp/ui/fregment/HomeFragment.kt
@@ -1,25 +1,28 @@
 package com.example.myapp.ui.fregment
 
+import android.app.ProgressDialog
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.example.myapp.R
 import com.example.myapp.databinding.FragmentHomeBinding
 import com.example.myapp.utils.NetworkResult
+import com.example.myapp.utils.toast
 import com.example.myapp.viewmodels.UserViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment() ,View.OnClickListener{
 
     private val binding by lazy { FragmentHomeBinding.inflate(layoutInflater) }
-
     private val viewModel by activityViewModels<UserViewModel>()
 
-
+//    var progressDialog: ProgressDialog? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,22 +45,41 @@ class HomeFragment : Fragment() {
         viewModel.getUserListRequest()
 
         initObese()
+
+        setOnClickListner()
+    }
+
+    private fun setOnClickListner() {
+        binding.btn.setOnClickListener(this)
     }
 
     private fun initObese() {
         viewModel.userResponseLiveData.observe(viewLifecycleOwner){
             when(it){
                 is NetworkResult.Success -> {
-                    Log.e("TAG", "initObeser: "+it.data?.data.toString() )
+
+                    binding.txtView.text = it.data?.data.toString()
+                    context?.toast({ "Done" })
                 }
                 is NetworkResult.Error -> {
-
+                    context?.toast({ "Errro" })
                 }
                 is NetworkResult.Loading ->{
-
+                    context?.toast({ "loading" })
                 }
             }
         }
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn -> {
+                viewModel.getUserListRequest()
+                context?.toast({ "request" })
+
+            }
+        }
+
     }
 
 
